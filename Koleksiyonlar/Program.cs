5cd143312985e4fc081622cde223d3d05eb11d90
Berat <berat2emre@gmail.com>
2021-01-20 23:53:54 +0300

    Proje dosyası ekle.

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            Console.WriteLine(list.Count);
+            Console.WriteLine(list.FindIndex(2));
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product
+            {
+                Id=1,
+                CategoryId = 1,
+                ProductName="Telefon",
+                UnitPrice=2000
+            };
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product);
+        }
+    }
+}
